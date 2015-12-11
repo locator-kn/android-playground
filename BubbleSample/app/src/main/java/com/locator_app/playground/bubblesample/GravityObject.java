e523c605407bbed34257256cff1f808d43ec481a
Markus Heilig <markusheilig@gmx.net>
2015-12-11 17:52:31 +0100

    implemented gravity

--- BubbleSample/app/src/main/java/com/locator_app/playground/bubblesample/GravityObject.java
+++ BubbleSample/app/src/main/java/com/locator_app/playground/bubblesample/GravityObject.java
@@ -5,42 +5,26 @@ import android.graphics.Point;
 
 public class GravityObject {
 
-    private Point center;
-    private int radius;
-    private float mass;
+    double x, y;
+    double vx, vy;
+    double radius, mass;
     private boolean fixedPosition;
 
-    public GravityObject(Point center, int radius, float mass, boolean fixedPosition) {
-        this.center = center;
-        this.radius = radius;
-        this.mass = mass;
+    public GravityObject(boolean fixedPosition) {
         this.fixedPosition = fixedPosition;
+        // todo determine smart initial values for velocity vector
+        this.vx = 0;
+        this.vy = 0;
     }
 
-    public int getRadius() {
-        return radius;
-    }
-
-    public Point getCenter() {
-        return center;
-    }
-
-    public float getMass() {
-        return mass;
-    }
-
-    public boolean hasFixedPosition() {
+    public boolean fixed() {
         return fixedPosition;
     }
 
-    void moveTo(int x, int y) {
-        center.set(x, y);
-    }
-
-    int distanceTo(GravityObject other) {
-        int xDelta = center.x - other.center.x;
-        int yDelta = center.y - other.center.y;
-        int distance = (int) Math.sqrt(xDelta * xDelta + yDelta * yDelta);
+    double distanceTo(GravityObject other) {
+        double xDelta = x - other.x;
+        double yDelta = y - other.y;
+        double distance = Math.sqrt(xDelta * xDelta + yDelta * yDelta);
         return distance;
     }
 }
--- BubbleSample/app/src/main/java/com/locator_app/playground/bubblesample/MainActivity.java
+++ BubbleSample/app/src/main/java/com/locator_app/playground/bubblesample/MainActivity.java
@@ -3,17 +3,26 @@ package com.locator_app.playground.bubblesample;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout mainLayout;
     BubbleView schoenhier;
     BubbleView userProfile;
-    RelativeLayout mainLayout;
+    Map<BubbleView, GravityObject> bubbles;
+    GravitySimulator simulator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,39 +36,136 @@ public class MainActivity extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         Point resolution = new Point();
         display.getSize(resolution);
-        schoenhier.moveTo(resolution.x / 2 + 100, resolution.y / 2 - 200);
-        userProfile.moveTo(resolution.x / 2, (int)(resolution.y * 0.7));
+        schoenhier.setInnerRadius(130);
+        schoenhier.moveTo(resolution.x / 2 + 20, resolution.y / 2 - 200);
+        schoenhier.loadImage("drawable://" + R.drawable.schoenhier);
+        userProfile.moveTo(resolution.x / 2, (int) (resolution.y * 0.76));
 
         schoenhier.setOnClickListener(onSchoenhierClicked);
         userProfile.setOnClickListener(onUserProfileClicked);
+
+        bubbles = new HashMap<>();
+        init();
     }
 
-    private View.OnClickListener onSchoenhierClicked = new View.OnClickListener() {
+    View.OnClickListener onSchoenhierClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Schön hier!", Toast.LENGTH_SHORT).show();
-            schoenhier.moveTo(0, 0);
+            go();
         }
     };
 
-    private View.OnClickListener onUserProfileClicked = new View.OnClickListener() {
+    View.OnClickListener onUserProfileClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "Updating profile", Toast.LENGTH_SHORT).show();
-            // addBubbleOnRuntime
+            go();
         }
     };
 
-    private void addBubbleOnRuntime() {
-        BubbleView bubble = BubbleBuilder.on(getApplicationContext())
-                .image("drawable://" + R.drawable.locatorlogo)
-                .innerColor(Color.RED)
-                .innerRadius(70)
-                .borderColor(Color.BLACK)
-                .innerRadius(5)
-                .shadowWidth(20)
+    void init() {
+        bubbles.put(schoenhier, toGravityObject(schoenhier, true));
+        bubbles.get(schoenhier).mass = 1000;
+        bubbles.put(userProfile, toGravityObject(userProfile, true));
+        bubbles.get(userProfile).mass = 20;
+
+        BubbleView b1 = BubbleBuilder.on(getApplicationContext())
+                .borderColor(color(R.color.borderBlue))
+                .innerColor(color(R.color.innerBlue))
+                .borderWidth(10)
+                .innerRadius(100)
+                .shadowWidth(10)
                 .position(100, 100)
                 .build();
-        mainLayout.addView(bubble);
+        mainLayout.addView(b1);
+        bubbles.put(b1, toGravityObject(b1, false));
+        BubbleView b2 = BubbleBuilder.on(getApplicationContext())
+                .borderColor(color(R.color.borderGreen))
+                .innerColor(color(R.color.innerGreen))
+                .borderWidth(10)
+                .innerRadius(50)
+                .shadowWidth(10)
+                .position(500, 50)
+                .build();
+        mainLayout.addView(b2);
+        bubbles.put(b2, toGravityObject(b2, false));
+        BubbleView b3 = BubbleBuilder.on(getApplicationContext())
+                .borderColor(color(R.color.borderYellow))
+                .innerColor(color(R.color.innerYellow))
+                .borderWidth(10)
+                .innerRadius(100)
+                .shadowWidth(10)
+                .position(0, 300)
+                .build();
+        mainLayout.addView(b3);
+        bubbles.put(b3, toGravityObject(b3, false));
+        BubbleView b4 = BubbleBuilder.on(getApplicationContext())
+                .borderColor(color(R.color.borderRed))
+                .innerColor(color(R.color.innerRed))
+                .borderWidth(10)
+                .innerRadius(75)
+                .shadowWidth(10)
+                .position(400, 300)
+                .build();
+        mainLayout.addView(b4);
+        bubbles.put(b4, toGravityObject(b4, false));
+        BubbleView b5 = BubbleBuilder.on(getApplicationContext())
+                .borderColor(color(R.color.borderPurple))
+                .innerColor(color(R.color.innerPurple))
+                .borderWidth(10)
+                .innerRadius(75)
+                .shadowWidth(10)
+                .position(100, 500)
+                .build();
+        mainLayout.addView(b5);
+        bubbles.put(b5, toGravityObject(b5, false));
+    }
+
+    private int color(int id) {
+        return ContextCompat.getColor(getApplicationContext(), id);
+    }
+
+    GravityObject toGravityObject(BubbleView bubble, boolean fixed) {
+        GravityObject gravityObject = new GravityObject(fixed);
+        gravityObject.x = bubble.getCenter().x;
+        gravityObject.y = bubble.getCenter().y;
+        gravityObject.radius = bubble.getRadius();
+        gravityObject.mass = bubble.getRadius();
+        return gravityObject;
+    }
+
+    void go() {
+        if (simulator == null) {
+            int w = mainLayout.getWidth();
+            int h = mainLayout.getHeight();
+            simulator = new GravitySimulator(1.0, w, h);
+        }
+
+        int i = 0;
+        do {
+            simulator.simulateGravity(bubbles.values(), 500);
+            if (i++ == 5)
+                break;
+        } while (anyCollision());
+        updateBubbles();
+    }
+
+    void updateBubbles() {
+        for (BubbleView bubble: bubbles.keySet()) {
+            GravityObject gravityObject = bubbles.get(bubble);
+            bubble.moveTo((float)gravityObject.x, (float)gravityObject.y);
+        }
+    }
+
+    boolean anyCollision() {
+        for (GravityObject o1: bubbles.values()) {
+            for (GravityObject o2: bubbles.values()) {
+                if (o1 == o2)
+                    continue;
+                double distance = o1.distanceTo(o2);
+                if (distance < o1.radius + o2.radius)
+                    return true;
+            }
+        }
+        return false;
     }
 }

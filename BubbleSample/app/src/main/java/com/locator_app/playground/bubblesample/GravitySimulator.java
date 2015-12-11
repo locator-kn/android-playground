f93cb272afa26c6d35a0e2226b90b3d01ddf88b4
Markus Heilig <markusheilig@gmx.net>
2015-12-11 11:59:41 +0100

    created GravitySimulator

---
+++ BubbleSample/app/src/main/java/com/locator_app/playground/bubblesample/GravitySimulator.java
@@ -0,0 +1,16 @@
+package com.locator_app.playground.bubblesample;
+
+
+import java.util.List;
+
+public class GravitySimulator {
+
+
+    public GravitySimulator() {
+
+    }
+
+    public void simulateGravity(List<GravityObject> objects) {
+        
+    }
+}
